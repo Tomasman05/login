d8ab26b203efb55e1172875974202eebc7768637
Vitovszki Tamas <lolozol80xd@gmail.com>
2023-06-09 08:34:55 +0200

    felh. mentese

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,26 @@ export class AppComponent {
   title = 'login';
   user!:string;
   pass!:string;
+  saveName!:boolean;
   login(){
     console.log(this.user)
     console.log(this.pass)
+    if(this.saveName){
+      this.deletePass()
+    }
+    else{this.deleteInputs()}
+  }
+  setCheck(){
+    if(this.saveName==true){
+      this.saveName=false
+    }
+    else{this.saveName=true}
+  }
+  deleteInputs(){
+    this.user=''
+    this.pass=''
+  }
+  deletePass(){
+    this.pass=''
   }
 }
